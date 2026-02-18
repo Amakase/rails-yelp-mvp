d85d7d9b0699421867e8814d8cebd12c94e16f1d
Steven Sawadisavi <stevesavi+lewagon@gmail.com>
2026-02-18 11:41:35 +0900

    implemented index silo

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,22 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+  end
+
+  private
+
+  def restuarant_params
   end
 end
